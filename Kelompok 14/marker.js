8f10d289cb6edcab29ad90785eb78026edfd3eb3
halimelsaputra <halim3@mhs.usk.ac.is>
2026-04-27 23:34:54 +0700

    feat: update longitude, langitude

--- Kelompok 14/marker.js
+++ Kelompok 14/marker.js
@@ -2,15 +2,15 @@
 const markersMilan = [
   {
     nama: "SMAN 3 Banda Aceh",
-    lat: 5.5485,
-    lng: 95.3235,
+    lat: 5.561178506812139,
+    lng: 95.33172850612229,
     deskripsi: "Sekolah unggulan",
     kategori: "sekolah"
   },
   {
     nama: "SMAN 1 Banda Aceh",
-    lat: 5.5562,
-    lng: 95.3176,
+    lat: 5.551604921721761,
+    lng: 95.31246333897619,
     deskripsi: "Sekolah favorit",
     kategori: "sekolah"
   }
@@ -20,15 +20,15 @@ const markersMilan = [
 const markersHalim = [
   {
     nama: "Masjid Raya Baiturrahman",
-    lat: 5.5539,
-    lng: 95.3170,
+    lat: 5.5537880145676555,
+    lng: 95.3176493506125,
     deskripsi: "Masjid ikonik",
     kategori: "masjid"
   },
   {
     nama: "Masjid Oman",
-    lat: 5.5500,
-    lng: 95.3300,
+    lat: 5.567473980688686,
+    lng: 95.33878995617626,
     deskripsi: "Masjid modern",
     kategori: "masjid"
   }
@@ -38,15 +38,15 @@ const markersHalim = [
 const markersReyan = [
   {
     nama: "Museum Tsunami",
-    lat: 5.5463,
-    lng: 95.3157,
+    lat: 5.548258458891555,
+    lng: 95.31529671384897,
     deskripsi: "Tempat bersejarah",
     kategori: "umum"
   },
   {
     nama: "Taman Sari",
-    lat: 5.5530,
-    lng: 95.3175,
+    lat: 5.546124850282711,
+    lng: 95.31595883049734,
     deskripsi: "Tempat santai",
     kategori: "umum"
   }

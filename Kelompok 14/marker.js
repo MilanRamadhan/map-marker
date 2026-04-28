d34e42853b533935a2bccc01ebf62297e207899e
milan <milan23@mhs.usk.ac.id>
2026-04-28 19:48:36 +0700

    update marker milan banda aceh

--- Kelompok 14/marker.js
+++ Kelompok 14/marker.js
@@ -5,15 +5,15 @@ const markersMilan = [
     lat: 5.561178506812139,
     lng: 95.33172850612229,
     deskripsi: "Sekolah unggulan",
-    kategori: "sekolah"
+    kategori: "sekolah",
   },
   {
     nama: "SMAN 1 Banda Aceh",
     lat: 5.551604921721761,
     lng: 95.31246333897619,
     deskripsi: "Sekolah favorit",
-    kategori: "sekolah"
-  }
+    kategori: "sekolah",
+  },
 ];
 
 // Halim (Masjid)
@@ -23,15 +23,15 @@ const markersHalim = [
     lat: 5.5537880145676555,
     lng: 95.3176493506125,
     deskripsi: "Masjid ikonik",
-    kategori: "masjid"
+    kategori: "masjid",
   },
   {
     nama: "Masjid Oman",
     lat: 5.567473980688686,
     lng: 95.33878995617626,
     deskripsi: "Masjid modern",
-    kategori: "masjid"
-  }
+    kategori: "masjid",
+  },
 ];
 
 // Reyan (Umum)
@@ -41,13 +41,13 @@ const markersReyan = [
     lat: 5.548258458891555,
     lng: 95.31529671384897,
     deskripsi: "Tempat bersejarah",
-    kategori: "umum"
+    kategori: "umum",
   },
   {
     nama: "Taman Sari",
     lat: 5.546124850282711,
     lng: 95.31595883049734,
     deskripsi: "Tempat santai",
-    kategori: "umum"
-  }
+    kategori: "umum",
+  },
 ];
